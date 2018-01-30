746018e3de7c1ddc9e8dfd6551298c362eb7ad74
cary <276524330@qq.com>
2018-01-30 17:57:35 +0800

    update order

---
+++ app/src/main/java/com/jiujiu/autosos/me/MyAccountActivity.java
@@ -0,0 +1,21 @@
+package com.jiujiu.autosos.me;
+
+import android.os.Bundle;
+
+import com.jiujiu.autosos.common.base.AbsBaseActivity;
+
+/**
+ * Created by Administrator on 2018/1/30.
+ */
+
+public class MyAccountActivity extends AbsBaseActivity {
+    @Override
+    protected void onActivityCreate(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutID() {
+        return 0;
+    }
+}
